830e69af7835025ca69c4554ebcceeda43a327b9
Vidya Prakash Pandey <prakashvidya368@gmail.com>
2025-09-05 23:08:52 +0530

    I fixed the Error And my program is running well now

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask,render_template
+from modules.readData import read_json_file
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    data = read_json_file('Database/dataset.json')
+    print(data)
+    return render_template('home.html',data=data)
 
 
 if __name__=="__main__":
